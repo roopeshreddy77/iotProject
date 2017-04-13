ba809f9d79e2ecd88c7b35dbe5e2ec27b02bac73
roopeshreddy77 <roopeshreddy77@gmail.com>
2017-04-12 20:23:53 -0700

    minor changes to front end

--- routes/display.js
+++ routes/display.js
@@ -3,15 +3,24 @@ var mysql = require('./mysql');
 /* GET home page. */
 exports.display=function(req, res, next) {
     var Query = "SELECT * FROM sensordata";
+    var Query1 = "SELECT * FROM sensordata1";
+    var result={};
     mysql.fetchData(function(err, results) {
         if (err) {
             throw err;
         } else {
-            console.log(JSON.stringify(results));
-            res.render('display', { results : results });
+          result.table1=results;
+          mysql.fetchData(function(err, results) {
+              if (err) {
+                  throw err;
+              } else {
+                result.table2=results;
+                  console.log(JSON.stringify(results));
+                  res.render('display', { results : result });
+              }
+          }, Query1);
+
         }
     }, Query);
 
 };
-
-
